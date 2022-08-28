28f73fc6281839648ef1101544c0fb5fcedc6faf
surya <suryakondisetti741@gmail.com>
2022-08-28 23:08:03 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,47 @@
+console.log("hello world");
+
+//variables
+
+ name='surya';
+console.log(name);
+
+
+
+//------------------------------------------------------------------------------------------
+//operator
+
+// username=prompt('Enter the username');
+// password=prompt('Enter the password');
+// console.log(username);
+// console.log(password);
+// alert('entered username : '+username +'entered password : '+password);
+
+
+//-----------------------------------------------------------------------------------------
+
+//operator
+
+// food=Number(prompt('enter the food amount?'));
+// tippercentage=Number(prompt('tip %?')) /100;
+// finalamount=food*tippercentage;
+// console.log(finalamount);
+// alert(finalamount);
+
+//------------------------------------------------------------------------------------------
+
+//operator
+//add +, sub -, multi *, modu %, exponents **, div /
+
+
+//--------------------------------------------------------------------------------------------
+
+//Math methods- m should be captial
+//floor -method to round down the things like floor(100.25)=100
+//celi - method to round up the things like celi(100.25)=101
+//random - method to pickup the numbers randomly
+
+
+
+
+
+
